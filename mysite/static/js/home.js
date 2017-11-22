67d1817bdc6f81a5323f3d833e9a48ead16e71e0
adarshhari.90@gmail.com <THINKPALM@TPO-TVMA-WKS003.TVMTHINKPALM.LAN>
2017-11-22 20:49:35 +0530

    commit

---
+++ mysite/static/js/home.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+	// body...
+	console.log("hereeeee")
+
+	$("#update_page_form").submit(function(e) {
+        e.preventDefault();
+    }).validate({
+        rules: {
+            about: {
+                required: true,
+                minlength: 5
+            },
+            phone: {
+                required: true,
+                minlength: 5
+            },
+            emails: {
+                required: true,
+                minlength: 5,
+            },
+            // street: {
+            //     required: true,
+            //     minlength: 3
+            // },
+            // city: {
+            //     required: true,
+            //     minlength: 2
+            // },
+            // country: {
+            //     required: true,
+            //     minlength: 2
+            // },
+            // zip: {
+            //     required: true,
+            //     minlength: 4
+            // }
+        },
+        submitHandler: function(form) { 
+            $.ajax({
+                url: '/update-page-info', 
+                type: 'POST',
+                data: $('#update_page_form').serialize(),
+                success: function(data){
+                    window.location = '/?st='+data['success']+'&em='+data['eum']
+                }
+            });
+            return false;  //This doesn't prevent the form from submitting.
+        }
+    });
+     $("#update").on('click', function() {
+        $("#update_page_form").submit();
+    });
+
+});
