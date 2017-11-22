ae484505335b222177b12d9a1c6411b20cfb99ec
adarshhari.90@gmail.com <THINKPALM@TPO-TVMA-WKS003.TVMTHINKPALM.LAN>
2017-11-22 20:59:46 +0530

    commit

--- fb/views.py
+++ fb/views.py
@@ -35,8 +35,10 @@ class FacebookPageManager(object):
 
     def update_page_info(self, data):
         # updates facebook page information
-        location =  '{'+'"city": "{city}", "street": "{street}", "state": "{state}", "country": "{country}", "zip": "{zip}"'.format(city=data.get('city'), street=data.get('street'),  state=data.get('state'), country=data.get('country'), zip=data.get('zip')) + '}'
-        payload = {'access_token': data.get('access_token'), 'about' : data.get('about',''),'phone' : data.get('phone',''), 'emails' : '["{}",]'.format(data.get('emails')), 'location' : location}
+        print (data, "??????????????????????????????????????????????????")
+        # location =  '{'+'"city": "{city}", "street": "{street}", "state": "{state}", "country": "{country}", "zip": "{zip}"'.format(city=data.get('city'), street=data.get('street'),  state=data.get('state'), country=data.get('country'), zip=data.get('zip')) + '}'
+        # payload = {'access_token': data.get('access_token'), 'about' : data.get('about',''),'phone' : data.get('phone',''), 'emails' : '["{}",]'.format(data.get('emails')), 'location' : location}
+        payload = {'access_token': data.get('access_token'), 'about' : data.get('about',''),'phone' : data.get('phone',''), 'emails' : '["{}",]'.format(data.get('emails'))}
         url = '{}/{}/'.format(self.API_ENDPOINT, data.get('id'))
         resp = requests.post(url, params = payload)
         print('Page update response ', resp.text)
